27d67b690f507e35f69310557ae7175fbcb75bc9
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-10-27 19:54:02 +0800

    [yyl]feat: add CrossOrigin

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/api/GroupController.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/api/GroupController.java
@@ -2,6 +2,7 @@ package com.thoughtworks.capability.gtb.entrancequiz.api;
 
 import com.thoughtworks.capability.gtb.entrancequiz.domain.Student;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class GroupController {
 
     private List<Student> students = initStudents();
